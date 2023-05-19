a4c783c9e637011780648720fe68820404f24704
Érico Andrei <ericof@gmail.com>
2023-05-19 10:39:07 +0200

    Add tests for Google and GitHub authentication

--- tests/creation/test_create_intranet_volto.py
+++ tests/creation/test_create_intranet_volto.py
@@ -3,10 +3,12 @@ from plone import api
 from plone.distribution.api import site as site_api
 from zope.component.hooks import setSite
 
+import json
 import pytest
 
 
 DISTRIBUTION_NAME = "intranet-volto"
+OAUTH_KEY_PREFIX = "pas.plugins.authomatic.interfaces.IPasPluginsAuthomaticSettings"
 
 
 class TestPloneAuth:
@@ -69,3 +71,84 @@ class TestPloneAuth:
         with api.env.adopt_user(user=nobody):
             user = api.user.get_current()
             assert api.user.has_permission(permission, user=user, obj=site) is expected
+
+
+class TestPloneGitHub:
+    @pytest.fixture(autouse=True)
+    def site_plone_auth(self, app):
+        answers_plone_auth = {
+            "site_id": "Intranet",
+            "title": "Intranet with GitHub Authentication",
+            "description": "An intranet with GitHub Authentication",
+            "default_language": "en",
+            "portal_timezone": "America/Sao_Paulo",
+            "setup_content": True,
+            "authentication": "GitHub",
+            "consumer_key": "foo",
+            "consumer_secret": "bar",
+        }
+        with api.env.adopt_roles(["Manager"]):
+            site = site_api.create(app, DISTRIBUTION_NAME, answers_plone_auth)
+            setSite(site)
+        self.site = site
+
+    @pytest.mark.parametrize(
+        "attr,expected",
+        [
+            ["id", "Intranet"],
+            ["title", "Intranet with GitHub Authentication"],
+            ["description", "An intranet with GitHub Authentication"],
+        ],
+    )
+    def test_site_attributes(self, attr, expected):
+        site = self.site
+        assert getattr(site, attr) == expected
+
+    def test_site_authomatic_settings(self):
+        json_config = json.loads(
+            api.portal.get_registry_record(f"{OAUTH_KEY_PREFIX}.json_config")
+        )
+        assert "github" in json_config
+        assert json_config["github"]["consumer_key"] == "foo"
+        assert json_config["github"]["consumer_secret"] == "bar"
+
+
+class TestPloneGoogle:
+    @pytest.fixture(autouse=True)
+    def site_plone_auth(self, app):
+        answers_plone_auth = {
+            "site_id": "Intranet",
+            "title": "Intranet with Google Authentication",
+            "description": "An intranet with Google Authentication",
+            "default_language": "en",
+            "portal_timezone": "America/Sao_Paulo",
+            "setup_content": True,
+            "authentication": "Google",
+            "consumer_key": "foo",
+            "consumer_secret": "bar",
+            "auth_scope": "['profile', 'email']",
+        }
+        with api.env.adopt_roles(["Manager"]):
+            site = site_api.create(app, DISTRIBUTION_NAME, answers_plone_auth)
+            setSite(site)
+        self.site = site
+
+    @pytest.mark.parametrize(
+        "attr,expected",
+        [
+            ["id", "Intranet"],
+            ["title", "Intranet with Google Authentication"],
+            ["description", "An intranet with Google Authentication"],
+        ],
+    )
+    def test_site_attributes(self, attr, expected):
+        site = self.site
+        assert getattr(site, attr) == expected
+
+    def test_site_authomatic_settings(self):
+        json_config = json.loads(
+            api.portal.get_registry_record(f"{OAUTH_KEY_PREFIX}.json_config")
+        )
+        assert "google" in json_config
+        assert json_config["google"]["consumer_key"] == "foo"
+        assert json_config["google"]["consumer_secret"] == "bar"
